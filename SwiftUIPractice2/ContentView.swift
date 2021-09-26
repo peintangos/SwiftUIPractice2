eba5ce5f4ae793844e88acf7bfbcb9fcd8274d65
JumpeiMatsuo <beatsjunpei@gmail.com>
2021-09-26 17:48:31 +0900

    Initial Commit

---
+++ SwiftUIPractice2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIPractice2
+//
+//  Created by 松尾淳平 on 2021/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIPractice2/SwiftUIPractice2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPractice2App.swift
+//  SwiftUIPractice2
+//
+//  Created by 松尾淳平 on 2021/09/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPractice2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
